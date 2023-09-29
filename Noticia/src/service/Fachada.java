baf4bcd57d35f4bfe3882469137cc4e141ce6e69
Haniel Costa da Silva <87027617+HanielCostaDaSilva@users.noreply.github.com>
2023-09-29 13:43:57 -0300

    modificações basicas fachada

--- Noticia/src/service/Fachada.java
+++ Noticia/src/service/Fachada.java
@@ -5,10 +5,10 @@ import java.util.List;
 import database.DAOAssunto;
 import database.DAONoticia;
 import database.DAOUsuario;
+import database.DAO;
 import model.Assunto;
 import model.Noticia;
 import model.Usuario;
-import util.Util;
 
 public class Fachada {
     private static DAONoticia daoNoticia = new DAONoticia();
@@ -16,60 +16,96 @@ public class Fachada {
     private static DAOUsuario daoUsuario = new DAOUsuario();
     public static Usuario logado;
 
-    public static Noticia adicionarNoticia(String titulo, String dataPublicacao, String link) {
+    public static Noticia adicionarNoticia(String titulo String dataPublicacao, String link)  throws Exception{
+        DAO.begin();
+        Noticia resultado= daoNoticia.read(titulo);
+        if (resultado!=null) throw new Exception("Noticia:" + titulo +"já cadastrado!") ;
 
         Noticia noticia = new Noticia(titulo, dataPublicacao, link);
         daoNoticia.create(noticia);
+        DAO.commit();
+
         return noticia;
     }
 
-    public static Assunto adicionarAssunto(String nome) {
+    public static Assunto adicionarAssunto(String nome) throws Exception{
+        DAO.begin();
+        Assunto resultado= daoAssunto.read(nome);
+        if (resultado!=null) throw new Exception("Assunto:" + nome +"já cadastrado!") ;
 
         Assunto assunto = new Assunto(nome);
         daoAssunto.create(assunto);
+        DAO.commit();
+
         return assunto;
     }
 
-    public static Usuario adicionarUsuario(String nickname, String password) {
+    public static Usuario adicionarUsuario(String nickname, String password) throws Exception {
 
-        Usuario usuario = new Usuario(nickname, password);
-        daoUsuario.create(usuario);
-        return usuario;
+        DAO.begin();
+		Usuario usu = daoUsuario.read(nickname);
+		if (usu!=null)
+			throw new Exception("Usuario ja cadastrado:" + nickname);
+		usu = new Usuario(nickname, password);
+
+		daoUsuario.create(usu);
+		DAO.commit();
+		return usu;
+
     }
 
     public static List<Noticia> listarNoticias() {
+        DAO.begin();
         List<Noticia> listaNoticia = daoNoticia.readAll();
+        DAO.commit();
         return listaNoticia;
 
     }
 
     public static List<Assunto> listarAssuntos() {
+        DAO.begin();
         List<Assunto> listaAssunto = daoAssunto.readAll();
+        DAO.commit();
         return listaAssunto;
     }
 
     public static List<Usuario> listarUsuarios() {
+        DAO.begin();
         List<Usuario> listaUsuario = daoUsuario.readAll();
+        DAO.commit();
         return listaUsuario;
     }
 
-    public static Noticia removerNoticia(int id) {
+    public static Noticia removerNoticia(int id) throws  Exception {
+        DAO.begin();
         Noticia noticia = daoNoticia.read(id);
+        if(noticia== null) throw new Exception("Noticia não encontrado!");
+
+        //if (noticia.)
+
+
         daoNoticia.delete(noticia);
+        DAO.commit();
         return noticia;
 
     }
 
-    public static Assunto removerAssunto(int id) {
+    public static Assunto removerAssunto(int id) throws  Exception {
+        DAO.begin();
         Assunto assunto = daoAssunto.read(id);
+        if(assunto== null) throw new Exception("Assunto não encontrado!");
         daoAssunto.delete(assunto);
+        DAO.commit();
         return assunto;
 
     }
 
-    public static Usuario removerUsuario(int id) {
+    public static Usuario removerUsuario(int id) throws  Exception {
+        DAO.begin();
         Usuario usuario = daoUsuario.read(id);
+        if(usuario== null) throw new Exception("Usuario não encontrado!");
         daoUsuario.delete(usuario);
+        DAO.commit();
         return usuario;
 
     }
@@ -94,20 +130,25 @@ public class Fachada {
 
     public static Usuario atualizarUsuario(int id, String nickname, String password) {
         Usuario usuario = daoUsuario.read(id);
-        usuario.setNickName(nickname);
+        usuario.setNickname(nickname);
         usuario.setPassword(password);
         daoUsuario.create(usuario);
         return usuario;
     }
 
-    public static void inicializar() {
-    }
+	public static void inicializar(){
+		DAO.open();
+	}
+	public static void finalizar(){
+		DAO.close();
+	}
 
-    public static void finalizar() {
-    }
-
-    public static Usuario localizarUsuario(String nome, String senha) {
-        return null;
-    }
-
+	public static Usuario localizarUsuario(String nickname, String password) {
+		Usuario usu = daoUsuario.read(nickname);
+		if (usu==null)
+			return null;
+		if (! usu.getPassword().equals(password))
+			return null;
+		return usu;
+	}
 }
